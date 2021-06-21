081d379331d31b29f7b4a949cbb506e5eb83f735
jaroslaw243 <boss.mielczarek@gmail.com>
2021-06-21 19:26:45 +0200

    kolejne poprawki

--- raport.R
+++ raport.R
@@ -10,7 +10,7 @@ slide <- function (x1, x2, x3, x4, x5)
 
   pres <- on_slide(pres, index = 2)
   paragraph2 <- fpar(ftext("Wartosci wlasne", fp_text(color = "black", font.size = 30, bold = TRUE)))
-  pres <- ph_with(pres, value = paragraph2, location = ph_location(left = 3.2,top = 0.7,width = 4,height = 0.5))
+  pres <- ph_with(pres, value = paragraph2, location = ph_location(left = 2,top = 0.7,width = 4,height = 0.5))
   pres <- ph_with(pres, value = x3, location = ph_location(left = 7.3,top = 1.5,width = 5,height = 4.5))
   pres <- ph_with(pres, value = x1, location = ph_location(left = 1,top = 2, width = 5.5,height = 4))
 
